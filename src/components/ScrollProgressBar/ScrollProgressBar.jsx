60e0ce1e6722de26268897d73756b4eb07c07ff9
NamHoai <hoainamsmurf@gmail.com>
2025-08-18 11:28:46 +0700

    update-progressbar

--- src/components/ScrollProgressBar/ScrollProgressBar.jsx
+++ src/components/ScrollProgressBar/ScrollProgressBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const ScrollProgressBar = () => {
   const [scrollPercent, setScrollPercent] = useState(0);
@@ -22,12 +22,15 @@ const ScrollProgressBar = () => {
     <div className="fixed top-0 left-0 right-0 z-[9999] pointer-events-none overflow-x-hidden">
       <div
         className={`
-          h-1
-          bg-gradient-to-r
-          from-blue-500 to-purple-500
-          dark:from-orange-500 dark:to-pink-600
-          transition-all duration-200 ease-out
-        `}
+      h-1
+      rounded-r-xl
+      transition-all duration-200 ease-out
+      opacity-0
+      bg-gradient-to-r
+      from-teal-400 to-cyan-500
+      dark:from-teal-300 dark:to-cyan-400
+      radius
+    `}
         style={{
           width: `${scrollPercent}%`,
           opacity: scrollPercent > 0 ? 1 : 0,
